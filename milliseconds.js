9492434632d0ca9729a7ce694f25f6ecb8741d99
Henrik Joreteg <hjoreteg@gmail.com>
2014-06-01 20:58:23 -0700

    add rounding

--- milliseconds.js
+++ milliseconds.js
@@ -1,9 +1,11 @@
+var round = Math.round;
+
 module.exports = {
-    seconds: function (val) { return val * 1000 },
-    minutes: function (val) { return val * 60000 },
-    hours: function (val) { return val * 3600000 },
-    days: function (val) { return val * 86400000 },
-    weeks: function (val) { return val * 604800000 },
-    months: function (val) { return val * 2629800000 },
-    years: function (val) { return val * 31557600000 }
+    seconds: function (n) { return round(n * 1000) },
+    minutes: function (n) { return round(n * 60000) },
+    hours: function (n) { return round(n * 3600000) },
+    days: function (n) { return round(n * 86400000) },
+    weeks: function (n) { return round(n * 604800000) },
+    months: function (n) { return round(n * 2629800000) },
+    years: function (n) { return round(n * 31557600000) }
 };
--- test.js
+++ test.js
@@ -2,6 +2,8 @@ var assert = require('assert');
 var ms = require('./milliseconds');
 
 assert.equal(ms.seconds(2), 2000);
+assert.equal(ms.seconds(1.11113213), 1111);
+assert.equal(ms.seconds(5.555555), 5556);
 assert.equal(ms.minutes(2), 120000);
 assert.equal(ms.hours(2), 7200000);
 assert.equal(ms.days(2), 172800000);
