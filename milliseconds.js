70d11715466f212969fb611df349cfc71f84bd03
Henrik Joreteg <hjoreteg@gmail.com>
2014-06-01 22:55:20 -0700

    a few little tweaks

--- milliseconds.js
+++ milliseconds.js
@@ -1,14 +1,12 @@
-var rounder = function(m) {
-    return function(n) {
-        return Math.round(n * m);
-    }
+function calc(m) {
+    return function(n) { return Math.round(n * m); };
 };
 module.exports = {
-    seconds: rounder(1000),
-    minutes: rounder(60000),
-    hours: rounder(3600000),
-    days: rounder(86400000),
-    weeks: rounder(604800000),
-    months: rounder(2629800000),
-    years: rounder(31557600000)
+    seconds: calc(1e3),
+    minutes: calc(6e4),
+    hours: calc(36e5),
+    days: calc(864e5),
+    weeks: calc(6048e5),
+    months: calc(26298e5),
+    years: calc(315576e5)
 };
